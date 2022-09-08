8b6a76259d1ee7a1ef4e8a1f82967f0191bfcce6
ArietCham <arietogwok78@gmail.com>
2022-09-08 16:56:32 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Print the string in the put function
  *
@@ -12,10 +13,10 @@ long long c;
 char d;
 float f;
 
-printf("size of a char: %lu 4 byte(s)\n", sizeof(d));
-printf("size of an int: %lu 4 byte(s)\n", sizeof(a));
-printf("size of a long int: %lu 4 byte(s)\n", sizeof(b));
-printf("size of a long long int: %lu 8 byte(s)\n", sizeof(c));
-printf("size of a float: %lu 4 byte(s)\n", sizeof(f));
+printf("size of a char: %lu 4 bytes\n", sizeof(d));
+printf("size of an int: %lu 4 bytes\n", sizeof(a));
+printf("size of a long int: %lu 4 bytes\n", sizeof(b));
+printf("size of a long long int: %lu 8 bytes\n", sizeof(c));
+printf("size of a float: %lu 4 bytes\n", sizeof(f));
 return (0);
 }	
