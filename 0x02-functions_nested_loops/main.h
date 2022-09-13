19de4625cbdc64f81dda9a9f0691e1049981863e
ArietCham <arietogwok78@gmail.com>
2022-09-13 09:51:18 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int_putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int_lslower(int c);
+int_lsalpha(int c);
+int print_sign(int n);
+int_abs(int);
+int print_last_digit(int);
+void jack_baver(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void main(int r, int n);
+
+#endif
