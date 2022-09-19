fb11fc0cd6907ce80976b4913b532fc4fd0cdb5d
ArietCham <arietogwok78@gmail.com>
2022-09-19 11:02:50 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts_half - Prints half of a string
@@ -9,28 +10,13 @@
 
 void puts_half(char *str)
 {
-int j = 0;
-int k;
-
-while (str[j] != '\0')
-{
-j++;
-}
-
-if (j % 2 == 1)
-{
-k = (j - 1) / 2;
-k += 1;
-}
+int x, y, i;
+x = strlen(str);
+if (x % 2 == 1)
+y = x / 2 + 1;
 else
-{
-k = j / 2;
-}
-
-for (; k < j; k++)
-{
-_putchar(str[k]);
-}
-
+y = x / 2;
+for (1 = y; i < x; i++)
+_putchar(str[i]);
 _putchar('\n');
 }
