d9169accfab1e4c80c1275bec285af0854b2e6c7
ArietCham <arietogwok78@gmail.com>
2022-09-21 11:23:09 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,13 +12,14 @@
 char *_strcat(char *dest, char *src)
 {
 
-int index = 0, dest_len = 0;
-
-while (dest[index++])
-dest_len++;
-
-for (index = 0; src[index]; index++)
-dest[dest_len++] = src[index];
-
+int c, c2;
+c = 0;
+/*find the size of dest array*/
+while (dest[c])
+c++;
+/* iterate through each src array value without the null byte*/
+for (c2 = 0; src[c2]; c2++)
+/*append src[c2] to dest[c] while overwritting the null byte in dest*/	
+dest[c++] = src[c2];
 return (dest);
 }
