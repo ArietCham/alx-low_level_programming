c13e8a0484a7e476d9c839ae24a3ad0fdab8661f
ArietCham <arietogwok78@gmail.com>
2022-09-23 14:14:57 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,25 +8,28 @@
  */
 char *cap_string(char *s)
 {
-int i = 0, j;
-char a[] = " \t\n,;.!?\"(){}";
+int index = 0;
 
-while (*(s + i))
+while (str[++index)
 {
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
-{
-if (i == 0)
-*(s + i) -= 'a' - 'A';
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == *(s + i - 1))
-*(s + i) -= 'a' - 'A';
+
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+
+if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}')
+	str[index] -= 32;
 }
-}
-}
-i++
-}
-return (s);
+return (str);
 }
