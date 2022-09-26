02b34a983eefc7c803f46d64d521c67422534303
ArietCham <arietogwok78@gmail.com>
2022-09-26 13:46:19 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
-
-	while (i < n)
-	{
-		*dest(dest + i) = *(src + i);
-		i++;
-	}
-	return (dest);
+unsigned int i = 0;
+while (i < n)
+{
+*dest(dest + i) = *(src + i);
+i++;
+}
+return (dest);
 }
