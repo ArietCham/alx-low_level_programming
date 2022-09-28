a59d7f05641ccf2daabda8536862a9e94966658e
ArietCham <arietogwok78@gmail.com>
2022-09-28 13:01:48 -0700

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 if (n == 1 || n == 0)
 return (n);
-return (_sqrt(0, n));
+return (sqrt(0, n));
 }
 
 /**
@@ -26,5 +26,5 @@ if (n > x / 2)
 return (-1);
 else if (n * n == x)
 return (n);
-return (_sqrt(n + 1, x));
+return (sqrt(n + 1, x));
 }
