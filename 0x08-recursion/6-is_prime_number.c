30603a1073ac911203002306ea5b930c0eab5863
ArietCham <arietogwok78@gmail.com>
2022-09-29 12:12:44 -0700

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,12 +12,12 @@ int helperFunction(int num, int i)
 if (i < num)
 {
 if (num % i == 0)
-{	
+{
 return (0);
 }
 else
 {
-return helperFunction(num, i + 1);
+return (helperFunction(num, i + 1));
 }
 }
 else
