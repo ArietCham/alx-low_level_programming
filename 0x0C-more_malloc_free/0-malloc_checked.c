21474ea1c2c76fc40cb6f2dfa815094f7f99ab96
ArietCham <arietogwok78@gmail.com>
2022-10-06 12:18:19 -0700

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - array for prints a string
@@ -10,7 +11,7 @@ void *malloc_checked(unsigned int b)
 {
 void *p;
 
-p - malloc(b);
+p = malloc(b);
 if (p == NULL)
 {
 exit(98);
