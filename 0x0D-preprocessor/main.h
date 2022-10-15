bcbf324011fe99b7a66ffc3fcf94ed52ad17a747
ArietCham <arietogwok78@gmail.com>
2022-10-15 14:56:28 -0700

    SUM

--- 0x0D-preprocessor/main.h
+++ 0x0D-preprocessor/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <0-object_like_macro.h>
-#include <1-pi.h>
-#include <2-main.c>
-#include <3-function_like_macro.h>
-#include <4-sum.h>
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include "2-main.c"
+#include "3-function_like_macro.h"
+#include "4-sum.h"
 
 #endif
