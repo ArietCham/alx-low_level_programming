3bae7002deb3c2196f2ec436f86b946a0aad83dc
ArietCham <arietogwok78@gmail.com>
2022-12-26 01:18:48 -0800

    task 0

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * @n: numbers of chars to be copied
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+
+while (dest[i++])
+j++;
+
+for (i = 0; src[i] && i < n; i++)
+dest[j++] = src[i];
+
+return (dest);
+}
